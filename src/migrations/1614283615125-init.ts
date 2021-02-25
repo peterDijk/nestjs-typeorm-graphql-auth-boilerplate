167a08585b8e24a3462607d7af98b78e30873042
Peter van Dijk <peter.van.dijk@bynder.com>
2021-02-25 21:09:26 +0100

    ormconfig reads ts files

--- src/migrations/1614283615125-init.ts
+++ src/migrations/1614283615125-init.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class pvdInit1614283070985 implements MigrationInterface {
-    name = 'pvdInit1614283070985'
+export class init1614283615125 implements MigrationInterface {
+    name = 'init1614283615125'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "health_checks" ("id" SERIAL NOT NULL, "message" text NOT NULL, "date_created" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), CONSTRAINT "PK_d44fff043c925e9cb7790950160" PRIMARY KEY ("id"))`);

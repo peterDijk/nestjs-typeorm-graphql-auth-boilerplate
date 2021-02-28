d3763dbde2367f66c05c3341da6d87f86a04a2eb
Peter van Dijk <peter.van.dijk@bynder.com>
2021-02-28 20:52:30 +0100

    protected gql route ok

--- src/health/health.resolver.ts
+++ src/health/health.resolver.ts
@@ -1,5 +1,7 @@
-import { Inject, Logger } from '@nestjs/common';
+import { Inject, Logger, UseGuards } from '@nestjs/common';
 import { Query, Resolver } from '@nestjs/graphql';
+import { CurrentUser, GqlAuthGuard } from '../auth/graphql.guard';
+import { User } from '../users/user.model';
 import { UsersService } from '../users/users.service';
 import { HealthCheck } from './health.model';
 import { HealthService } from './health.service';
@@ -13,9 +15,10 @@ export class HealthCheckResolver {
   private readonly logger = new Logger(HealthCheckResolver.name);
 
   @Query((returns) => [HealthCheck])
-  async healthchecks(): Promise<HealthCheck[]> {
+  @UseGuards(GqlAuthGuard)
+  async healthchecks(@CurrentUser() user: User): Promise<HealthCheck[]> {
     const healths = await this.healthService.findAll();
-    this.logger.log(healths, 'gethealthchecks');
+    this.logger.log({ user, healths }, 'gethealthchecks');
     return healths;
   }
 }
